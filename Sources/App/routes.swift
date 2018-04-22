df6a611071e5b2423fff4b806a93721cd6491b6d
Brian Mullen <bmullen@ancestry.com>
2018-04-22 13:40:22 -0600

    Add additional routes

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -9,10 +9,27 @@ public func routes(_ router: Router) throws {
     router.get("hello") { req in
         return "Hello, world!"
     }
+    router.get("hello", "vapor") { req -> String in
+        return "Hello Vapor!"
+    }
+    router.get("hello", String.parameter) { req -> String in
+        let name = try req.parameters.next(String.self)
+        return "Hello, \(name)!"
+    }
+    router.post(InfoData.self, at: "info") { req, data -> InfoResponse in
+        return InfoResponse(data: data)
+    }
 
     // Example of configuring a controller
     let todoController = TodoController()
     router.get("todos", use: todoController.index)
     router.post("todos", use: todoController.create)
     router.delete("todos", Todo.parameter, use: todoController.delete)
+}
+
+struct InfoData: Content {
+    let name: String
+}
+struct InfoResponse: Content {
+    let data: InfoData
 }
